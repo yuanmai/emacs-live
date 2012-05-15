77240aca8e955dbc6bd6dea7a092e9d77db3f8d5
Yuan Mai <4registration@gmail.com>
2012-05-15 22:33:41 +0800

    ignore live

--- packs/live/power-pack/config/yasnippet-conf.el
+++
@@ -1,8 +0,0 @@
-(live-add-pack-lib "yasnippet")
-(require 'yasnippet)
-(yas/initialize)
-
-(setq yas/root-directory (concat live-etc-dir "snippets"))
-
-;; Load the snippets
-(yas/load-directory yas/root-directory)
